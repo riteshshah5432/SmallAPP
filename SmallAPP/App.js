afdd357a6701dda4355f9d8a03a2b1be4225e1bd
Ritesh Shah <>
2023-06-09 20:22:02 +0530

    - Using the javascript - Remove the hermes because of it's not supported in xcode

---
+++ SmallAPP/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
